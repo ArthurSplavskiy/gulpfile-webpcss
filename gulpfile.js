c89aac29c8d911842c104578433f12cc59f5e19d
Arthur <gitmail10@gmail.com>
2020-10-04 22:24:29 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,197 @@
+let project_folder = require("path").basename(__dirname); // build folder
+let source_folder = "dev";
+
+let fs = require("fs");
+
+let path = {
+  build: {
+    html: project_folder + "/",
+    css: project_folder + "/css",
+    js: project_folder + "/js",
+    img: project_folder + "/img",
+    fonts: project_folder + "/fonts",
+  },
+  dev: {
+    html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
+    css: source_folder + "/scss/main.scss",
+    js: source_folder + "/js/script.js",
+    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+    fonts: source_folder + "/fonts/*.{ttf,woff2}",
+  },
+  watch: {
+    html: source_folder + "/**/*.html",
+    css: source_folder + "/scss/**/*.scss",
+    js: source_folder + "/js/**/*.js",
+    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+  },
+  clean: "./" + project_folder + "/",
+};
+
+let { src, dest } = require("gulp"),
+  gulp = require("gulp"),
+  browsersync = require("browser-sync").create(),
+  fileinclude = require("gulp-file-include"),
+  del = require("del"),
+  scss = require("gulp-sass"),
+  autoprefixer = require("gulp-autoprefixer"),
+  group_media = require("gulp-group-css-media-queries"),
+  clean_css = require("gulp-clean-css"),
+  rename = require("gulp-rename"),
+  uglify = require("gulp-uglify-es").default,
+  imagemin = require("gulp-imagemin"),
+  webp = require("gulp-webp"),
+  webphtml = require("gulp-webp-html"),
+  ttf2woff2 = require("gulp-ttf2woff2"), // Скачуємо шрифти і розміщаємо в dev/fonts з розширенням .ttf
+  fonter = require("gulp-fonter"), // Форматує інші типи шрифтів з папки dev/fonts в розширенням .ttf (task tottf -> запуск gulp tottf)
+  webpcss = require("gulp-webp-css");
+
+function browserSync(params) {
+  browsersync.init({
+    server: {
+      baseDir: "./" + project_folder + "/",
+    },
+    port: 3000,
+    notify: false,
+  });
+}
+
+function html() {
+  return src(path.dev.html)
+    .pipe(fileinclude())
+    .pipe(webphtml())
+    .pipe(dest(path.build.html))
+    .pipe(browsersync.stream());
+}
+
+function css() {
+  return src(path.dev.css)
+    .pipe(
+      scss({
+        outputStyle: "expanded",
+      })
+    )
+    .pipe(group_media())
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 5 versions"],
+        cascade: true,
+      })
+    )
+    .pipe(webpcss())
+    .pipe(dest(path.build.css))
+    .pipe(clean_css())
+    .pipe(
+      rename({
+        extname: ".min.css",
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream());
+}
+
+function js() {
+  return src(path.dev.js)
+    .pipe(fileinclude())
+    .pipe(dest(path.build.js))
+    .pipe(uglify())
+    .pipe(
+      rename({
+        extname: ".min.js",
+      })
+    )
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream());
+}
+
+function images() {
+  return src(path.dev.img)
+    .pipe(
+      webp({
+        quality: 70,
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(src(path.dev.img))
+    .pipe(
+      imagemin({
+        progressive: true,
+        svgoPlugins: [{ removeViewBox: false }],
+        interlaced: true,
+        optimizationLevel: 3, // 0 to 7
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream());
+}
+
+function fonts() {
+  return src(path.dev.fonts).pipe(ttf2woff2()).pipe(dest(path.build.fonts));
+}
+
+gulp.task("tottf", function () {
+  return src([source_folder + "/fonts/*.{otf, eot}"]) // розширення шрифтів які потрібно форматувати
+    .pipe(
+      fonter({
+        formats: ["ttf"], // розширення у яке форматується
+      })
+    )
+    .pipe(dest(source_folder + "/fonts/"));
+});
+
+function fontsStyle(params) {
+  let file_content = fs.readFileSync(source_folder + "/scss/_fonts.scss");
+  if (file_content == "") {
+    fs.writeFile(source_folder + "/scss/_fonts.scss", "", cb);
+    return fs.readdir(path.build.fonts, function (err, items) {
+      if (items) {
+        let c_fontname;
+        for (var i = 0; i < items.length; i++) {
+          let fontname = items[i].split(".");
+          fontname = fontname[0];
+          if (c_fontname != fontname) {
+            fs.appendFile(
+              source_folder + "/scss/_fonts.scss",
+              '@include font("' +
+                fontname +
+                '", "' +
+                fontname +
+                '", "400", "normal");\r\n',
+              cb
+            );
+          }
+          c_fontname = fontname;
+        }
+      }
+    });
+  }
+}
+
+function cb() {}
+
+function watchFiles(params) {
+  gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.css], css);
+  gulp.watch([path.watch.js], js);
+  gulp.watch([path.watch.img], images);
+}
+
+function clean() {
+  return del(path.clean);
+}
+
+let build = gulp.series(
+  clean,
+  gulp.parallel(js, css, html, images, fonts),
+  fontsStyle
+);
+let watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.fontsStyle = fontsStyle;
+exports.fonts = fonts;
+exports.images = images;
+exports.js = js;
+exports.css = css;
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
